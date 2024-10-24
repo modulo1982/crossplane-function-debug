8a949dc2940ae634c33ae4e2bca48650f46d892e
tkanters <kanters.t@tkppensioen.nl>
2024-10-24 14:49:19 +0200

    fix: linting

--- function/fn.py
+++ function/fn.py
@@ -1,7 +1,7 @@
 """A Crossplane composition function."""
 
 import grpc
-from prettyprinter import pformat
+from prettyprinter import pprint
 
 from crossplane.function import logging, resource, response
 from crossplane.function.proto.v1 import run_function_pb2 as fnv1
@@ -10,20 +10,23 @@ from google.protobuf.json_format import MessageToDict
 
 
 class FunctionRunner(grpcv1.FunctionRunnerService):
+    """A FunctionRunner handles gRPC RunFunctionRequests."""
 
     def __init__(self):
+        """Create a new FunctionRunner."""
         self.log = logging.get_logger()
 
     async def RunFunction(
         self, req: fnv1.RunFunctionRequest, _: grpc.aio.ServicerContext
     ) -> fnv1.RunFunctionResponse:
+        """Run the function."""
         log = self.log.bind(tag=req.meta.tag)
         log.info("Called with RunFunctionRequest:")
 
         if req.input and "spec" in req.input:
-            print(pformat(MessageToDict(req, preserving_proto_field_name=True), depth=req.input["spec"]["depth"]))
+            pprint(MessageToDict(req, preserving_proto_field_name=True), depth=req.input["spec"]["depth"])
         else:
-            print(pformat(MessageToDict(req, preserving_proto_field_name=True)))
+            pprint(MessageToDict(req, preserving_proto_field_name=True))
 
         rsp = response.to(req)
         return rsp
